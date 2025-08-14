2fafe8f9f6dcc71e9f610d4933b9dd637fa06fbf
Nasir-afridi <nasirwc05@gmail.com>
2025-08-14 13:30:03 +0500

    Added the input and button of the chatbot

---
+++ React-Practice/src/ChatBot/Chatbot.jsx
@@ -0,0 +1,11 @@
+function ChatInput() {
+    return(
+        <>
+             <input placeholder="Send a message to Chatbot"/>
+            <button>Send</button>
+        </>
+       
+    );
+}
+
+export default ChatInput

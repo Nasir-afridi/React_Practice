c38da3b16ea71455ea9a089e5a5d4d5d83b79719
Nasir-afridi <nasirwc05@gmail.com>
2025-08-14 15:40:30 +0500

    added the image of the robot and clean up the code

--- React-Practice/src/ChatBot/ChatMessage.jsx
+++ React-Practice/src/ChatBot/ChatMessage.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 
 const ChatMessage = (props) => {
     const message = props.message
+    const sender = props.sender
+
+    // if(sender === 'robot') {
+    //     return (
+    //     <div>
+    //         <img src="/robot.png" width={50} alt="" />
+    //         {message}
+    //     </div>
+    //     )
+    // }
+
+
   return (
     <div>
+        {sender === 'robot' && <img src="/robot.png" width={50} />}
+        {sender === 'user' && <img src="/user.png" width={50} />}
         {message}
-        <img src="/user.png" width={50} alt="" />
     </div>
   )
 }

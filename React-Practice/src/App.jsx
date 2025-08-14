1b7bf2a406306bde488b66ba0ff1c28c8e698fe2
Nasir-afridi <nasirwc05@gmail.com>
2025-08-14 17:27:26 +0500

    import the second component

--- React-Practice/src/App.jsx
+++ React-Practice/src/App.jsx
@@ -1,6 +1,7 @@
 import First from "./Exercises/Lesson-01"
 import ChatInput from "./ChatBot/ChatInputs"
 import ChatMessage from "./ChatBot/ChatMessage"
+import Second from "./Exercises/Lesson-02-Comp&Props"
 
 function App() {
 
@@ -12,9 +13,7 @@ function App() {
       <ChatMessage message = 'Hi how can i help you' sender = 'robot'/>
       <ChatMessage message = 'can you get me today date' sender = 'user'/>
       <ChatMessage message = 'Today is August 14' sender = 'robot'/> 
-
-
-
+      <Second />
     </>
   )
 }
